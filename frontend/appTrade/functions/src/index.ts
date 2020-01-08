94a97943f8440a4d1f5ae77f468fc9e6ed24e8a5
alexelfandi <alexelfandi60@gmail.com>
2020-01-08 12:09:20 +0100

    pruebas firebase
    pruebas firebase

--- frontend/appTrade/functions/src/index.ts
+++ frontend/appTrade/functions/src/index.ts
@@ -1,8 +1,19 @@
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+//import { database, firestore } from 'firebase-admin';
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+ // Start writing Firebase Functions
+ // https://firebase.google.com/docs/functions/typescript
+
+ export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+ });
+
+ exports.simpleDbFunction = functions.database.ref('/path')
+ .onCreate((snap, context) => {
+   if (context.authType === 'ADMIN') {
+     console.log("All done");
+     
+   } 
+ });
