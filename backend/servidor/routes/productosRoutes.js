9f1a9d34d8387cea874d5250e399a001d8ec7ffb
alvaro5monge <Alvarom201999@gmail.com>
2020-01-12 19:13:03 +0100

    Primer WriteHead

--- backend/servidor/routes/productosRoutes.js
+++ backend/servidor/routes/productosRoutes.js
@@ -13,6 +13,7 @@ listaProductos.push({ "id": 5, "nombre": "Plata", "imagenes": [], "descripcion":
 listaProductos.push({ "id": 5, "nombre": "Plata", "imagenes": [], "descripcion": "Un Plata muy bonito", "valor": 280 });
 
 router.get(`/lista`, (req, res) => {
+    res.writeHead(200,{'content-Type':'text/plain'});//No estoy seguro...Alvaro
     res.send(listaProductos);
 });
 
