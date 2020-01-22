8c3fd705a38c941083c638d3e9dbeca920ab155d
AlejandroTorres243 <39425653+AlejandroTorres243@users.noreply.github.com>
2020-01-22 19:27:23 +0100

    SUBIENDO CAMBIOS

--- backend/servidor-nest/src/publicacion-module/publicacion-controller/publicacion.controller.ts
+++ backend/servidor-nest/src/publicacion-module/publicacion-controller/publicacion.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { Publicacion } from '../publicacion.entity';
+import { PublicacionService } from '../publicacion-service/publicacion.service';
 
 @Controller('publicacion')
-export class PublicacionController {}
+export class PublicacionController {
+
+    constructor(private readonly myService : PublicacionService){}
+
+    @Get('publicacion')
+    getDatos(): Promise <Publicacion[]> {
+        return this.myService.BuscalosTodos();
+    }
+    @Post()
+    async create(@Body() dato: Publicacion) {
+        return this.myService.Crear(dato);
+    }
+    @Put(':id')
+    async update(@Param('id') id: number, @Body() publicacion: Publicacion) {
+      await this.myService.BuscarporId(id);
+      publicacion.id = id;
+      return this.myService.Crear(publicacion);
+    }
+    @Delete(':id')
+    remove(@Param('id') id: number) {
+      return this.myService.Eliminar(id);
+    }
+}
