4671a4793dfa3a624707ada797b6ccf29adbe742
alvaro5monge <alvarom201999@gmail.com>
2020-01-22 11:57:00 +0100

    UserController

--- backend/servidor-nest/src/user-module/user-controller/user.controller.ts
+++ backend/servidor-nest/src/user-module/user-controller/user.controller.ts
@@ -1,4 +1,12 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+constructor(private readonly userService){}
+
+
+
+
+
+}
--- backend/servidor-nest/src/user-module/user-service/user.service.ts
+++ backend/servidor-nest/src/user-module/user-service/user.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) { }
+
+    findAll(): Promise<User[]> {
+        return this.userRepository.find();
+    }
+
+    save(newUser : User): Promise<User> {
+        return this.userRepository.save(newUser);
+    }
+
+    async findById(id: number): Promise<User> {
+        return this.userRepository.findOne(id);
+    }
+
+    async delete(id: number): Promise<User> {
+        const promesaObjeto = await this.userRepository.findOne(id);
+        return this.userRepository.remove(promesaObjeto);
+    }
+}
