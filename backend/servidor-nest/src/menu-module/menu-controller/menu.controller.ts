e58d4b1ccd657c380c355fffcd25153b6ecb1759
alvaro5monge <alvarom201999@gmail.com>
2020-01-22 15:48:24 +0100

    Menu Controller y service (sinProbar)

--- backend/servidor-nest/src/menu-module/menu-controller/menu.controller.ts
+++ backend/servidor-nest/src/menu-module/menu-controller/menu.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { MenuService } from '../menu-service/menu.service';
+import { Menu } from '../menu.entity';
 
 @Controller('menu')
-export class MenuController {}
+export class MenuController {
+
+    constructor(private readonly menuService : MenuService){}
+
+
+    @Get()
+    findAll():Promise<Menu[]>{
+        return this.menuService.findAll();
+    }
+
+    @Post()
+    create(@Body() menu : Menu):Promise<Menu>{
+        return this.menuService.save(menu);
+    }
+
+    @Get(':id')
+    findById(@Param() id : number):Promise<Menu>{
+        return this.menuService.findById(id);
+    }
+
+    @Put(':id')
+    async modifyById(@Param()id:number,@Body()menu : Menu):Promise<Menu>{
+        let menuEncontrado=await this.menuService.findById(id);
+        
+        menuEncontrado.titulo=menu.titulo;
+        menuEncontrado.publicado=menu.publicado;
+        menuEncontrado.autor=menu.autor;
+        menuEncontrado.icono=menu.icono;
+        menuEncontrado.enlaces=menu.enlaces;
+        menuEncontrado.main=menu.main;
+        menuEncontrado.publico=menu.publico;
+        menuEncontrado.url=menu.url;
+        //Ultimo editor
+        
+        return this.menuService.save(menuEncontrado);
+    }
+
+    @Delete(":id")
+    async borrarPorId(@Param()id :number):Promise<Menu>{
+        return this.menuService.delete(id);
+    }
+
+
+
+
+
+
+}
--- backend/servidor-nest/src/menu-module/menu-service/menu.service.ts
+++ backend/servidor-nest/src/menu-module/menu-service/menu.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Menu } from '../menu.entity';
+
 
 @Injectable()
-export class MenuService {}
+export class MenuService {
+
+    constructor(@InjectRepository(Menu) private readonly menuRepository : Repository<Menu>){}
+
+    findAll(): Promise<Menu[]> {
+        return this.menuRepository.find();
+    }
+    save(newMenu : Menu): Promise<Menu> {
+        return this.menuRepository.save(newMenu);
+    }
+
+    async findById(id: number): Promise<Menu> {
+        return this.menuRepository.findOne(id);
+    }
+
+    async delete(id: number): Promise<Menu> {
+        const promesaObjeto = await this.menuRepository.findOne(id);
+        return this.menuRepository.remove(promesaObjeto);
+    }
+
+
+}
