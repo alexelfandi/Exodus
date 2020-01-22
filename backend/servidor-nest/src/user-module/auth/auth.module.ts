a8ac89c409385a40c44a61f5966c48254ed39ecb
alexelfandi <alexelfandi60@gmail.com>
2020-01-22 11:40:06 +0100

    Revert "prueba inject repoitory"
    This reverts commit 0f1482d084e0dcf8f273890737d1116a35d9702b.

--- backend/servidor-nest/src/user-module/auth/auth.module.ts
+++ backend/servidor-nest/src/user-module/auth/auth.module.ts
@@ -2,23 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './authService/auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 import { UserService } from '../user-service/user.service';
-import { AuthController } from './auth-controller/auth.controller';
 // import { JwtStrategy } from './jwt.strategy';
+
 
 @Module({
   imports: [
-    PassportModule.register({ defaultStrategy: 'jwt',}),
+    PassportModule.register({ defaultStrategy: 'jwt',  }),
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '300s' },
     })
     ],
-  providers: [AuthService, LocalStrategy, UserService, JwtService],
-  controllers: [AuthController],
-  exports: [AuthModule, AuthService]
+  providers: [AuthService, LocalStrategy, UserService],
+  exports: [AuthService]
 })
 export class AuthModule {}
--- backend/servidor-nest/src/user-module/user-service/user.service.ts
+++ backend/servidor-nest/src/user-module/user-service/user.service.ts
@@ -1,18 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '../user.entity';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
 
-    constructor(
-        @InjectRepository(User)
-        private readonly userRepository: Repository<User>,
-    ){}
 
-    FindAll(): Promise<User[]>{
-        return this.userRepository.find()
+
+    FindAll(){
+
     }
     findOne(username: string){
 
