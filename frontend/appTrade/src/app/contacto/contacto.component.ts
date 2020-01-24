83b3cb5022ed023853122cf36600c3f03d2d8051
AlejandroTorres243 <39425653+AlejandroTorres243@users.noreply.github.com>
2020-01-24 16:10:10 +0100

    Servicios de contacto estan listo papu
    ahora me voy para el css

--- frontend/appTrade/src/app/contacto/contacto.component.ts
+++ frontend/appTrade/src/app/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contacto } from '../dominio/contacto';
 
 @Component({
   selector: 'app-contacto',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactoComponent implements OnInit {
 
-  Nombre: string = "Alex";
+  dato: Contacto[] = [];
+
   constructor() { }
 
   ngOnInit() {
